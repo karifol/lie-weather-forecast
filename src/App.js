68ca0f2a75898eb7a03419503551b32bde989163
karifol <karigolhorinouchi@gmail.com>
2023-12-23 18:46:40 +0900

    bild

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import NameCard from './components/NameCard/NameCard';
 import Forecast from './components/Forecast/Forecast';
 import Map from './components/Map/Map';
 import { useState } from 'react';
